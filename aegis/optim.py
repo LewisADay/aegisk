7b43d0cdbd97601e11f2fa7e113f8fc750e2f4de
LewisADay <lewis.alen.day@gmail.com>
2022-07-12 10:06:55 +0100

    Refactoring AsyncBO

--- aegis/optim.py
+++ aegis/optim.py
@@ -222,6 +222,9 @@ class AsyncBO:
         # add to the completed jobs counter
         self.n_completed += len(completed_jobs)
 
+    def _update_acq(self):
+        self.acq.update(self.model, self.ue.get())
+
     def _create_and_submit_jobs(self):
         # only submit up to the budget
         n_to_submit = np.minimum(
@@ -232,7 +235,7 @@ class AsyncBO:
             return
 
         # update the acquisition function ready for use.
-        self.acq.update(self.model, self.ue.get())
+        self._update_acq()
 
         # get locations to evaluate from the acquisition function, create a
         # job, and submit it
@@ -252,7 +255,7 @@ class AsyncBO:
 
         self.n_submitted += n_to_submit
 
-    def step(self):
+    def _setup_step(self):
         if self.verbose:
             print(
                 f" Submitted: {self.n_submitted} / {self.budget}"
@@ -262,7 +265,7 @@ class AsyncBO:
         # if the optimisation is complete, do nothing.
         if self.finished:
             print("finished")
-            return
+            return True
 
         # run the jobs until we there's one free
         if self.n_submitted < self.budget:
@@ -284,23 +287,45 @@ class AsyncBO:
         # if we've submitted our budget's worth of jobs,
         # do not submit any more
         if self.n_submitted == self.budget:
-            return
+            return True
 
-        # scale the output
-        T_out = self.output_transform(self.Ytr)
-        train_y = T_out.scale_mean(self.Ytr)
-        train_x = self.Xtr
+        # If we've got here we are setup and do
+        # not need to end this step
+        return False
 
-        # fit the gp
-        self.model, self.likelihood = gp.create_and_fit_GP(
+    def _setup_gp(self, train_x, train_y):
+
+        # Scale outputs
+        T = self.output_transform(train_y)
+        train_y = T.scale_mean(train_y)
+
+        # Fit gp
+        model, likelihood = gp.create_and_fit_GP(
             train_x=train_x,
             train_y=train_y,
             ls_bounds=self.ls_bounds,
             out_bounds=self.out_bounds,
             n_restarts=10,
-            verbose=self.verbose,
+            verbose=self.verbose
         )
+
+        return model, likelihood
+
+    def _setup_model_gp(self):
+        self.model, self.likelihood = self._setup_gp(self.Xtr, self.Ytr)
         self.model.eval()
+
+    def step(self):
+        
+        # Setup the step
+        skip_step = self._setup_step()
+
+        # If we need to end this step, do
+        if skip_step:
+            return
+
+        # Train model gp
+        self._setup_model_gp()
 
         # submit jobs
         self._create_and_submit_jobs()
@@ -322,7 +347,7 @@ class AsyncBO:
     def get_models(self):
         return self.model
 
-class AsyncTimeAcqBO:
+class AsyncTimeAcqBO(AsyncBO):
     def __init__(
         self,
         func,
@@ -335,56 +360,23 @@ class AsyncTimeAcqBO:
         q=1,
         time_func=time_dists.halfnorm(),
         verbose=False,
-        interface="job",
-        time_acq=True,
-        selective_killing=False
+        interface="job"
     ):
-        self.f = func
-        self.Xtr = Xtr
-        self.Ytr = Ytr
-        self.acq_class = acq_class
-        self.acq_params = acq_params
-        self.budget = budget
-        self.n_workers = n_workers
-        self.q = q
-        self.output_transform = getattr(transforms, "Transform_Standardize",)
-        self.time_func = time_func
-        self.verbose = verbose
-        self.time_acq_flag = time_acq
-        self.selective_killing_flag = selective_killing
 
-        interfaces = {
-            "job": executor.SimExecutorJumpToCompletedJob(
-                n_workers=n_workers, time_func=time_func, verbose=verbose
-            ),
-            "job-dependant": executor.SimExecutorJumpToCompletedJobProblemDependant(
-                n_workers=n_workers, time_func=time_func, verbose=verbose
-            )
-        }
-
-        self.interface = interfaces[interface]
-
-        self.dtype = Ytr.dtype
-
-        self.ue = util.UnderEval(self.n_workers, self.f.dim, self.dtype)
-
-        # time storage
-        self.time = torch.zeros_like(Ytr, dtype=self.dtype)
-        self.time_taken = torch.zeros_like(Ytr, dtype=self.dtype)
-
-        # counters
-        self.n_submitted = Xtr.shape[0]
-        self.n_completed = Xtr.shape[0]
-
-        # current iteration number
-        self.init_size = Xtr.shape[0]
-
-        # bounds
-        self.ls_bounds = [1e-4, np.sqrt(Xtr.shape[1])]
-        self.out_bounds = [1e-4, 10]
-
-        # acquisition function, initialised after we have built a model
-        self._acq = None
+        AsyncBO.__init__(
+            self,
+            func,
+            Xtr,
+            Ytr,
+            acq_class,
+            acq_params,
+            budget,
+            n_workers,
+            q=1,
+            time_func=time_dists.halfnorm(),
+            verbose=False,
+            interface="job"
+        )
 
         # sample time function for time of initialised points Xtr
         for i in range(self.time_taken.shape[0]):
@@ -396,202 +388,42 @@ class AsyncTimeAcqBO:
     @property
     def acq(self):
         if self._acq is None:
-            if self.time_acq_flag:
-                self._acq = self.acq_class(
-                    model=self.model,
-                    time_model=self.time_model,
-                    T_data=self.output_transform(self.Ytr),
-                    T_time=self.output_transform(self.time_taken),
-                    lb=self.f.lb,
-                    ub=self.f.ub,
-                    under_evaluation=self.ue.get(),
-                    **self.acq_params,
-                )
-            else:
-                self._acq = self.acq_class(
-                    model=self.model,
-                    lb=self.f.lb,
-                    ub=self.f.ub,
-                    under_evaluation=self.ue.get(),
-                    **self.acq_params,
-                )
+            self._acq = self.acq_class(
+                model=self.model,
+                time_model=self.time_model,
+                T_data=self.output_transform(self.Ytr),
+                T_time=self.output_transform(self.time_taken),
+                lb=self.f.lb,
+                ub=self.f.ub,
+                under_evaluation=self.ue.get(),
+                **self.acq_params,
+            )
 
         return self._acq
 
-    @property
-    def finished(self):
-        return self.n_completed == self.budget
+    def _update_acq(self):
+        self.acq.update(self.model, self.ue.get(), self.time_model)
 
-    def _process_completed_jobs(self):
-        # get the completed jobs
-        completed_jobs = self.interface.get_completed_jobs()
-        n = len(completed_jobs)
-
-        # storage, so we only cat once
-        _Xtr = torch.zeros((n, self.f.dim), dtype=self.dtype)
-        _Ytr = torch.zeros(n, dtype=self.dtype)
-        _time_taken = torch.zeros(n, dtype=self.dtype)
-        _time = torch.zeros(n, dtype=self.dtype)
-
-        # add the results to the training data
-        for i, job in enumerate(completed_jobs):
-            _Xtr[i] = job["x"]
-            _Ytr[i] = job["y"]
-            _time_taken[i] = torch.as_tensor(job["t"], dtype=self.dtype)
-            _time[i] = self.interface.status["t"]
-
-            # remove from under evaluation
-            self.ue.remove(_Xtr[i])
-
-            if self.verbose:
-                print(
-                    f"Completed -> f(x): {_Ytr[i]:0.3f}",
-                    f"time taken: {_time_taken[i]:0.3f}",
-                )
-
-        # store the results
-        self.Xtr = torch.cat((self.Xtr, _Xtr))
-        self.Ytr = torch.cat((self.Ytr, _Ytr))
-        self.time_taken = torch.cat((self.time_taken, _time_taken))
-        self.time = torch.cat((self.time, _time))
-
-        # add to the completed jobs counter
-        self.n_completed += len(completed_jobs)
-
-    def _create_and_submit_jobs(self):
-        # only submit up to the budget
-        n_to_submit = np.minimum(
-            self.budget - self.n_submitted,  # total left to submit
-            self.interface.status["n_free_workers"],  # free workers
-        )
-        if n_to_submit < 1:
-            return
-
-        # update the acquisition function ready for use.
-        if self.time_acq_flag:
-            self.acq.update(self.model, self.ue.get(), self.time_model)
-        else:
-            self.acq.update(self.model, self.ue.get())
-
-        # get locations to evaluate from the acquisition function, create a
-        # job, and submit it
-        for _ in range(n_to_submit):
-            # get the next location to evaluate
-            x = self.acq.get_next()
-
-            # create the job and submit it
-            job = {"x": x, "f": self.f}
-            self.interface.add_job_to_queue(job)
-
-            # add to under evaluation
-            self.ue.add(x)
-
-            if self.verbose:
-                print("Submitted ->", x.numpy().ravel())
-
-        self.n_submitted += n_to_submit
-
-    def _check_and_kill_jobs(self):
-
-        # Get list of evaluations we wish to kill
-        to_kill = self.acq.get_kill()
-
-        # If we wish to kill of evaluations
-        if to_kill is not None:
-            # For each evalauation we wish to kill
-            for x in to_kill:
-                # Kill that evaluation and remove it from
-                # UnderEval
-                self.interface.kill_job(x)
-                self.ue.remove(x)
-
-        return to_kill is not None
-
+    def _setup_cost_gp(self):
+        self.cost_model, self.cost_likelihood = self._setup_gp(self.Xtr, self.time_taken)
 
     def step(self):
-        if self.verbose:
-            print(
-                f" Submitted: {self.n_submitted} / {self.budget}"
-                f" Completed: {self.n_completed} / {self.budget}"
-            )
 
-        # if the optimisation is complete, do nothing.
-        if self.finished:
-            print("finished")
+        # Setup the step
+        skip_step = self._setup_step()
+
+        # If we need to end this step, do
+        if skip_step:
             return
 
-        # run the jobs until we there's one free
-        if self.n_submitted < self.budget:
-            self.interface.run_until_n_free(self.q)
+        # Train model gp
+        self._setup_model_gp()
 
-        # if we've submitted our budget's worth of jobs, keep waiting
-        # until one more worker each time is free. i.e. 1, 2, ..., n_workers
-        else:
-            self.interface.run_until_n_free(
-                self.n_workers - (self.budget - self.n_completed) + 1
-            )
-
-        if self.verbose:
-            print("Time:", self.interface.status["t"])
-
-        # get the completed jobs
-        self._process_completed_jobs()
-
-        # if we've submitted our budget's worth of jobs,
-        # do not submit any more
-        if self.n_submitted == self.budget:
-            return
-
-
-        # Problem gp
-        # scale the output
-        T_out = self.output_transform(self.Ytr)
-        train_y = T_out.scale_mean(self.Ytr)
-        train_x = self.Xtr
-
-        # fit the gp
-        self.model, self.likelihood = gp.create_and_fit_GP(
-            train_x=train_x,
-            train_y=train_y,
-            ls_bounds=self.ls_bounds,
-            out_bounds=self.out_bounds,
-            n_restarts=10,
-            verbose=self.verbose,
-        )
-        self.model.eval()
-
-        if self.time_acq_flag:
-            # Time gp
-            # scale the output
-            Time_out = self.output_transform(self.time_taken)
-            train_time = Time_out.scale_mean(self.time_taken)
-
-            # fit time gp
-            self.time_model, self.time_likelihood = gp.create_and_fit_GP(
-                train_x=train_x,
-                train_y=train_time,
-                ls_bounds=self.ls_bounds,
-                out_bounds=self.out_bounds,
-                n_restarts=10,
-                verbose=self.verbose,
-            )
-            self.time_model.eval()
+        # Train cost gp
+        self._setup_cost_gp()
 
         # submit jobs
         self._create_and_submit_jobs()
-
-        # If we are using a selective killing method
-        if self.selective_killing_flag:
-
-            # Check if we wish to kill any evaluations off
-            # and kill those evaluations returns true if one 
-            # or more evaluations were killed off
-            killed = self._check_and_kill_jobs()
-
-            # If we killed any evaluations off, reasign
-            # those workers
-            if killed:
-                self._create_and_submit_jobs()
 
         if self.verbose:
             print("------------------------------")
